84c95ca85eba9d25bc3fd58f1de831c6c3717439
bonsai phawakhot <bonza1230@gmail.com>
2025-09-19 15:43:08 +0700

    guard'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainLayout from "../layouts/MainLayout.vue";
 import HomePage from "../views/HomePage.vue";
+import {useAuthStore} from "@/stores/auth.store"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
@@ -16,6 +17,7 @@ const router = createRouter({
           alias: "/home",
           meta: {
             title: "Home - E-Slip",
+            requiresAuth: true 
           },
         },
         {
@@ -50,6 +52,7 @@ const router = createRouter({
       component: () => import("../views/Register.vue"),
       meta: {
         title: "Register - E-Slip",
+        requiresGuest: true
       },
     },
     {
@@ -58,6 +61,7 @@ const router = createRouter({
       component: () => import("../views/Login.vue"),
       meta: {
         title: "Login - E-Slip",
+        requiresGuest: true
       },
     },
     {
@@ -93,6 +97,20 @@ router.beforeEach((to, from, next) => {
   if (to.meta.title) {
     document.title = to.meta.title;
   }
+
+  const auth = useAuthStore() 
+  const token = auth.isAuthenticated
+
+  // 🔹 ป้องกันเข้า login ถ้ามี token อยู่แล้ว
+  if (to.meta.requiresGuest && token) {
+    return next('/')
+  }
+
+  // 🔹 ป้องกันเข้าหน้า protected ถ้าไม่มี token
+  if (to.meta.requiresAuth && !token) {
+    return next('/login')
+  }
+
   next();
 });
 
